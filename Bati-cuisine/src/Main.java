d636e1fda914574dfdcfda816b1171fd4aee9f41
mouadHILLALI <mauadtoto001@gmail.com>
2024-09-19 17:33:06 +0100

    BC-5 add materials

--- Bati-cuisine/src/Main.java
+++ Bati-cuisine/src/Main.java
@@ -1,8 +1,8 @@
+import configuration.DatabaseConnection;
 import controllers.MenuController;
 
 public class Main {
     public static void main(String[] args) {
-        MenuController controller = new MenuController();
-        controller.MenuController();
+        new MenuController();
     }
 }
